bb9ec31a4b489e97aba97a38b5d84f291de74955
Merlin Nimier-David <merlin.nimier@gmail.com>
2013-09-28 20:16:54 +0200

    Maps : affichage de cercles en cercle autour d'un cercle

--- js/maps.js
+++ js/maps.js
@@ -16,7 +16,7 @@
     /*********** Creating custom Map Type **************/
     var graphTypeOptions = {
         getTileUrl: function(coord, zoom) {
-            return "./images/maps_background.jpg"
+            return "./images/maps_background.jpg";
         },
         tileSize: new google.maps.Size(256, 256),
         maxZoom: MAX_ZOOM,
@@ -40,17 +40,15 @@
                 mapOptions);
         map.mapTypes.set('graph', graphMapType);
         map.setMapTypeId('graph');
-        drawCircle(myLatlng, 20, '#FF0000', map);
+        drawCircle(myLatlng, 20, '#3498db', map);
+
+        drawCirclesAround(8, myLatlng, 0.001, 20, '#d35400', map);
     }
 
     
     function drawCircle(center, rad, color, map) {
-        
-        
         var CircleOptions = {
-            strokeColor: color,
-            strokeOpacity: 1,
-            strokeWeight: 2,
+            strokeWeight: 0,
             fillColor: color ,
             fillOpacity: 1,
             map: map,
@@ -58,6 +56,30 @@
             radius: rad
         };
         Circle = new google.maps.Circle(CircleOptions);
+    }
+
+    function drawCirclesAround(numberOfCircles, center, globalRadius, circleRadius, circleColor, map) {
+
+        var maxAngle = (3/2) * Math.PI,
+            angularOffset = ((2*Math.PI) - maxAngle) / 2 - Math.PI / 2;
+
+        // On va décrire un grand cercle,
+        // et placer les marqueurs à intervalle régulier
+        for (var i = 0; i < numberOfCircles; i++) {
+            var progress = 0.5;
+            if (numberOfCircles > 1)
+                progress = i / (numberOfCircles - 1);
+            var lat = center.lat(),
+                lng = center.lng(),
+                dLat = globalRadius * Math.sin(progress * maxAngle + angularOffset),
+                dLng = globalRadius * Math.cos(progress * maxAngle + angularOffset);
+            var thisCenter = new google.maps.LatLng(lat + dLat, lng + dLng);
+            drawCircle(thisCenter, circleRadius, circleColor, map)
+        }
+
+        // On dézoom la map afin de voir au moins globalRadius
+        // TODO : zoom intelligent ? Ou bien zoom statique bien choisi
+        map.setZoom(MAX_ZOOM - 2);
     }
     
     
