79a1af04a3a6a9f522b2383343d3b996f6f284ce
Merlin Nimier-David <merlin.nimier@gmail.com>
2013-09-28 14:24:36 +0200

    Algorithme de parcours du graphe et affichage textuel

--- js/data.js
+++ js/data.js
@@ -65,9 +65,49 @@
 	}
 
 	function goThroughGraph(graph){
-		for(var i in graph){
-			console.log(graph[i]);
+		if (graph.length > 0)
+			buildGraph(graph[0], graph, 0);
+		else
+			console.log("Le graphe passé est vide.");
+	}
+
+	function buildGraph(currentNode, graph, depth){
+		// On affiche le noeud en cours
+		if (currentNode.position === undefined){
+			displayNode(currentNode, graph, depth);
 		}
+
+		// Pour chaque arrête partant de ce noeud
+		for (var j in currentNode.edges){
+			var edge = currentNode.edges[j];
+
+			// On récupère l'objet node qui représente la cible de cette connexion
+			// TODO : vraie fonction de recherche
+			var targetNode = graph[edge.to-1];
+			// On affiche récursivement la suite du graphe à partir de cette cible
+			buildGraph(targetNode, graph, depth+1);
+
+			// Et on dessine le lien entre le noeud courant et la cible
+			// TODO
+		}
+	}
+
+	function computeCoordinatesForNode(node, graph, depth){
+		return { lat: Math.random(), lng: Math.random() };
+	}
+
+	function displayNode(node, graph, depth){
+		var representation = "";
+		for (var i = 0; i < depth; i++){
+			representation += "    ";
+		}
+		representation += node.word;
+
+		// On enregistre les coordonnées à laquelles le noeud a été affiché
+		node.position = computeCoordinatesForNode(node, graph, depth);
+
+		$("#dump").text($("#dump").text() + "\n" + representation);
+		console.log("Mot courant : " + node.word);
 	}
 
 })(jQuery);
