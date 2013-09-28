aee1bc77a048af26ace7d3192b09c28019ea87e6
Vadim Caen <Vadim Caen@asus-perso.mshome.net>
2013-09-28 15:05:09 +0200

    Layer grey working

--- js/maps.js
+++ js/maps.js
@@ -1,25 +1,45 @@
-(function($){
-	
-	$(document).ready(function(){
-		init();
-        
-	});
+(function($) {
 
-	function init(){
-		initialize_map();
-        
-	}
-    
+    /* __INIT__ **/
+    $(document).ready(function() {
+        init();
+
+    });
+
+    function init() {
+        initialize_map();
+    }
+
+
+    /*********** Creating custom Map Type **************/
+    var graphTypeOptions = {
+        getTileUrl: function(coord, zoom) { return "./images/maps_background.jpg"},
+        tileSize: new google.maps.Size(256, 256),
+        maxZoom: 20,
+        minZoom: 0,
+        radius: 1738000,
+        name: 'Graph'
+    };
+    var graphMapType = new google.maps.ImageMapType(graphTypeOptions);
+
     function initialize_map() {
+        var myLatlng = new google.maps.LatLng(0, 0);
         var mapOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
-          zoom: 8,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
+            center: myLatlng,
+            zoom: 1,
+            streetViewControl: false,
+            mapTypeControlOptions: {
+                mapTypeIds: ['graph']
+            }
         };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-            
-      }
+
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+                mapOptions);
+        map.mapTypes.set('graph', graphMapType);
+        map.setMapTypeId('graph');
+
+    }
+
 
 
 })(jQuery);
